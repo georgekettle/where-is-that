1fdfddeeb5fcf81808f8784c9e4f51903d47cea4
george kettle <georgekettle.gk@gmail.com>
2021-11-11 14:00:37 +1100

    root path changed for logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
 
   devise_for :users
+  authenticated do
+    root to: 'pins#home', as: :authenticated_root
+  end
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
