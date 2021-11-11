f53616799e33a122e20ef38d1e9cd78a956489f4
george kettle <georgekettle.gk@gmail.com>
2021-11-11 18:02:47 +1100

    Basic profile layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def formatted_username
+    "@#{username}"
+  end
 end
