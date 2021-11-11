a0868c050b712a8b70003057319d2d2b029c55a3
george kettle <georgekettle.gk@gmail.com>
2021-11-11 13:53:21 +1100

    Navbar added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_235631) do
+ActiveRecord::Schema.define(version: 2021_11_10_235926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2021_11_10_235631) do
     t.index ["location_id"], name: "index_pins_on_location_id"
   end
 
+  create_table "saves", force: :cascade do |t|
+    t.bigint "board_id", null: false
+    t.bigint "pin_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_saves_on_board_id"
+    t.index ["pin_id"], name: "index_saves_on_pin_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,4 +62,6 @@ ActiveRecord::Schema.define(version: 2021_11_10_235631) do
   end
 
   add_foreign_key "pins", "locations"
+  add_foreign_key "saves", "boards"
+  add_foreign_key "saves", "pins"
 end
